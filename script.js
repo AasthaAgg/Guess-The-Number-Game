ce9a171ad8acec4b17f844a605d821196732d5da
Aastha Aggarwal <aastha.7090@gmail.com>
2022-10-22 12:10:49 +0530

    added play again button

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 var myNum;
 var msg = document.getElementById('msg');
 var noOfGuess = 0;
+var btnPlay = document.getElementById('play');
+var btnPlayAgain = document.getElementById('play_again');
 
 generateRandomNum();
+
+// ===== GENERATE RANDOM NUMBER =====
 
 function generateRandomNum(){
     myNum = Math.floor(Math.random()*100)+1;
 }
 
+// ===== HANDLE ALL INPUTS =====
+
 function play(){
     var inputNum = Number(document.getElementById('input_number').value);
+
+    // ===== IF INPUT IS OUT OF RANGE (1-100)
 
     if(inputNum < 1 || inputNum > 100){
         alert("Please Enter a number Between 1 to 100");
@@ -17,6 +25,8 @@ function play(){
     
     else{
         document.getElementById('no_of_guesses').textContent = ++noOfGuess;
+
+        // ===== IF THE GUESSED NUMBER IS LARGER =====
 
         if(inputNum > myNum){
             if(inputNum - myNum > 10){
@@ -27,6 +37,9 @@ function play(){
                 msg.style.color = "coral";
             }
         }
+
+        // ===== IF THE GUESSED NUMBER IS SMALLER =====
+
         else if(inputNum < myNum){
             if(myNum - inputNum > 10){
                 msg.textContent = "Your guess is Too Low";
@@ -36,9 +49,14 @@ function play(){
                 msg.style.color = "coral";
             }
         }
+
+        // ===== IF THE GUESSED NUMBER IS RIGHT =====
+
         else{
             msg.textContent = "Yeah! You guessed it right!🏆";
             msg.style.color = "green";
+            btnPlay.style.display = "none";
+            btnPlayAgain.style.display = "inline";
         }
     }
 }
